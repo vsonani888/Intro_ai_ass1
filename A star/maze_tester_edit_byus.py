8d22c1cfa3bbe39014289281ab8a2ab00c1f79cb
vsonani <152912926+vsonani888@users.noreply.github.com>
2025-02-25 19:52:34 -0500

    rewrite for maze tester

--- A star/maze_tester_edit_byus.py
+++ A star/maze_tester_edit_byus.py
@@ -69,14 +69,85 @@ def forward_maze_runner(maze_filename, tie_break):
 
     return num_solvable, total_expanded, total_time, total_mazes
 
+def backward_maze_runner(maze_filename, tie_break):
+    mazes = load_maze(maze_filename)
+
+    num_solvable = 0
+    total_expanded = 0
+    total_mazes = len(mazes)
+
+    start_time = time.time()
+
+    for maze in mazes:
+        start = None
+        goal = None
+
+        rows = len(maze)
+        cols = len(maze[0])
+
+        for i in range(rows):
+            for j in range(cols):
+                if maze[i][j] == 'A': #its agent cell
+                    start = (i, j)
+                elif maze[i][j] == 'T': #its target cell
+                    goal = (i, j)
+
+        A_star_output = repeated_backward_a_star(maze, start, goal, tie_break) 
+
+        path = A_star_output[0]
+        expanded_nodes = A_star_output[1]
+
+        if path: #path exists
+            num_solvable += 1
+            total_expanded += expanded_nodes
+
+    end_time = time.time()
+    total_time = end_time - start_time
+
+    return num_solvable, total_expanded, total_time, total_mazes
+
+def adaptive_maze_runner(maze_filename, tie_break):
+    mazes = load_maze(maze_filename)
+
+    num_solvable = 0
+    total_expanded = 0
+    total_mazes = len(mazes)
+
+    start_time = time.time()
+
+    for maze in mazes:
+        start = None
+        goal = None
+
+        rows = len(maze)
+        cols = len(maze[0])
+
+        for i in range(rows):
+            for j in range(cols):
+                if maze[i][j] == 'A': #its agent cell
+                    start = (i, j)
+                elif maze[i][j] == 'T': #its target cell
+                    goal = (i, j)
+
+        A_star_output = adaptive_a_star(maze, start, goal) 
+
+        path = A_star_output[0]
+        expanded_nodes = A_star_output[1]
+
+        if path: #path exists
+            num_solvable += 1
+            total_expanded += expanded_nodes
+
+    end_time = time.time()
+    total_time = end_time - start_time
+
+    return num_solvable, total_expanded, total_time, total_mazes
+
 
 
 if __name__ == "__main__":
-    print("hi from main")
 
     maze_filename = 'generated_mazes_101.txt'
-    total_tests_run = 50
-    tests_passed = 0
 
     #part 2 forward a star smaller G vs larger G
 
@@ -100,10 +171,47 @@ if __name__ == "__main__":
 
 
     print("Part 2 results:")
-    print(f"Smaller G block expansion: {expanded_sg} | total time: {time_sg:.4f}s")
-    print(f"Larger G block expansion: {expanded_lg} | total time: {time_lg:.4f}s")
+    print(f"Smaller G block expansion: {expanded_sg} | total time: {time_sg:.4f}s | solved : {solvable_sg}/{total_mazes_sg}")
+    print(f"Larger G block expansion: {expanded_lg} | total time: {time_lg:.4f}s | solved : {solvable_lg}/{total_mazes_lg}")
 
-    #part 3 forward a star smaller G vs larger G
+    #part 3 forward a star larger G vs backward a star larger g
+
+    #forward A* larger G
+    tie_break = 'LARGER_G'
+    result_f = forward_maze_runner(maze_filename, tie_break)
+
+    solvable_f = result_f[0]
+    expanded_f = result_f[1]
+    time_f = result_f[2]
+    total_mazes_f = result_f[3]
+
+    #backward A* larger G
+    tie_break = 'LARGER_G'
+    result_b = backward_maze_runner(maze_filename, tie_break)
+
+    solvable_b = result_b[0]
+    expanded_b = result_b[1]
+    time_b = result_b[2]
+    total_mazes_b = result_b[3]
+
+    print("Part 3 results:")
+    print(f"Forward A* larger G block expansion: {expanded_f} | total time: {time_f:.4f}s | solved : {solvable_f}/{total_mazes_f}")
+    print(f"Backward A* larger G block expansion: {expanded_b} | total time: {time_b:.4f}s | solved : {solvable_b}/{total_mazes_b}")
+
+    #part 5 forward a star larger G vs adaptive a star
+
+    #adaptive a star
+    result_a = adaptive_maze_runner(maze_filename, tie_break)
+
+    solvable_a = result_a[0]
+    expanded_a = result_a[1]
+    time_a = result_a[2]
+    total_mazes_a = result_a[3]
+
+    print("Part 3 results:")
+    print(f"Forward A* larger G block expansion: {expanded_f} | total time: {time_f:.4f}s | solved : {solvable_f}/{total_mazes_f}")
+    print(f"Backward A* larger G block expansion: {expanded_a} | total time: {time_a:.4f}s | solved : {solvable_a}/{total_mazes_a}")
+    
 
     
 
